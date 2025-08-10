7e20da656f8dd37f923b68d9546603210a10b195
ikjotkour27 <ikjotkourmultani.27@gmail.com>
2025-08-10 10:13:38 +0530

    added requests

--- zetawa-main/src/components/Contact.jsx
+++ zetawa-main/src/components/Contact.jsx
@@ -24,11 +24,11 @@ const Contact = () => {
     e.preventDefault();
     console.log("Form data submitted:", formData);
       try {
-        const response = await fetch('http://localhost:5000/contact', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(formData),
-        });
+        const response = await fetch('https://zetawa-dark-backend.onrender.com/contact', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
 
         const result = await response.json();
 
