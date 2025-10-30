da71738c9786e78f1965e6574f661b537ce103e2
ikjotkour27 <ikjotkourmultani.27@gmail.com>
2025-10-30 12:02:56 +0530

    go to top

--- zetawa-main/src/App.jsx
+++ zetawa-main/src/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import MainPage from './components/MainPage';
 import AdminProfile from './components/AdminProfile';
 import Certifications from './components/Certifications';
@@ -7,29 +7,33 @@ import Contact from './components/Contact';
 import Careers from './components/Careers';
 import Events from './components/Events';
 import PressRelease from './components/PressRelease';
-// import Applyform from './components/Applyform';
 import Hireforms from './components/Hireforms';
-import AboutUs from './components/AboutUs'
+import AboutUs from './components/AboutUs';
 
+// Scroll-to-top helper component
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+};
 
 const App = () => {
   return (
     <Router>
+      <ScrollToTop />
       <div className="App">
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/admin-profile" element={<AdminProfile />} />
-          {/* <Route path="/employee-details" element={<EmployeeDetails />} /> */}
           <Route path="/certifications" element={<Certifications />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/careers" element={<Careers />} />
           <Route path="/events" element={<Events />} />
-          <Route path="/press-release" element={<PressRelease />} /> 
-          <Route path="/hireforms" element={<Hireforms />}/>
-          <Route path="/aboutus" element={<AboutUs />}/>
-
-          {/* <Route path="/applyforms" element={<Applyform />}/> */}
-
+          <Route path="/press-release" element={<PressRelease />} />
+          <Route path="/hireforms" element={<Hireforms />} />
+          <Route path="/aboutus" element={<AboutUs />} />
         </Routes>
       </div>
     </Router>
